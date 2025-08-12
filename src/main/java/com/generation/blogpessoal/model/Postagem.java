d86048fa1bdf4557f97f9ecc50297b85a16a65ec
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-12 02:01:53 -0300

    :sparkles: Implementação do Recurso Usuario, Relacionamento com Postagem e Spring Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -23,20 +25,28 @@ public class Postagem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 		
+	@Column(length = 100)
 	@NotBlank(message = "O atributo título é obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 5 e no máximo 100 caracteres.")
+	@Pattern(regexp = "^[^0-9].*", message = "O título não pode ser apenas numérico")
 	private String titulo;
 	
+	@Column(length = 1000)
 	@NotBlank(message = "O atributo texto é obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no minimo 10 e no máximo 1000 caracteres.")
+	@Pattern(regexp = "^[^0-9].*", message = "O texto não pode ser apenas numérico")
 	private String texto;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
-    @ManyToOne
+
+	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 	
 	public Long getId() {
 		return id;
@@ -69,13 +79,21 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
+
 	public Tema getTema() {
-        return this.tema;
-    }
+		return tema;
+	}
 
-    public void setTema(Tema tema) {
-        this.tema = tema;
-    }
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
 }
